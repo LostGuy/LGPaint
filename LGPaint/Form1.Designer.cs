d17eec91bf8b65a6b6bdb68cb060e1b4a0959567
Jesse West <jwest46@uncc.edu>
2016-03-13 22:33:49 -0400

    Added the ability to change the brush size and color as well as opening an image.

--- LGPaint/Form1.Designer.cs
+++ LGPaint/Form1.Designer.cs
@@ -31,11 +31,22 @@
             this.clearButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
             this.canvas = new System.Windows.Forms.Panel();
+            this.changeColorButton = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.toolsHeader = new System.Windows.Forms.Label();
+            this.widthTrackBar = new System.Windows.Forms.TrackBar();
+            this.brushWidth = new System.Windows.Forms.Label();
+            this.brushHeightLabel = new System.Windows.Forms.Label();
+            this.heightTrackBar = new System.Windows.Forms.TrackBar();
+            this.openButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.widthTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightTrackBar)).BeginInit();
             this.SuspendLayout();
             // 
             // clearButton
             // 
-            this.clearButton.Location = new System.Drawing.Point(829, 475);
+            this.clearButton.Location = new System.Drawing.Point(835, 533);
             this.clearButton.Name = "clearButton";
             this.clearButton.Size = new System.Drawing.Size(81, 52);
             this.clearButton.TabIndex = 0;
@@ -45,7 +56,7 @@
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(829, 533);
+            this.exitButton.Location = new System.Drawing.Point(946, 533);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(81, 52);
             this.exitButton.TabIndex = 1;
@@ -63,17 +74,105 @@
             this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
             this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
             // 
+            // changeColorButton
+            // 
+            this.changeColorButton.Location = new System.Drawing.Point(857, 35);
+            this.changeColorButton.Name = "changeColorButton";
+            this.changeColorButton.Size = new System.Drawing.Size(148, 35);
+            this.changeColorButton.TabIndex = 3;
+            this.changeColorButton.Text = "Change Color";
+            this.changeColorButton.UseVisualStyleBackColor = true;
+            this.changeColorButton.Click += new System.EventHandler(this.changeColorButton_Click);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.FullOpen = true;
+            // 
+            // toolsHeader
+            // 
+            this.toolsHeader.AutoSize = true;
+            this.toolsHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolsHeader.Location = new System.Drawing.Point(911, 12);
+            this.toolsHeader.Name = "toolsHeader";
+            this.toolsHeader.Size = new System.Drawing.Size(38, 13);
+            this.toolsHeader.TabIndex = 4;
+            this.toolsHeader.Text = "Tools";
+            // 
+            // widthTrackBar
+            // 
+            this.widthTrackBar.Location = new System.Drawing.Point(876, 89);
+            this.widthTrackBar.Maximum = 100;
+            this.widthTrackBar.Minimum = 1;
+            this.widthTrackBar.Name = "widthTrackBar";
+            this.widthTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.widthTrackBar.TabIndex = 5;
+            this.widthTrackBar.Value = 1;
+            this.widthTrackBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.widthTrackBar_MouseUp);
+            // 
+            // brushWidth
+            // 
+            this.brushWidth.AutoSize = true;
+            this.brushWidth.Location = new System.Drawing.Point(897, 73);
+            this.brushWidth.Name = "brushWidth";
+            this.brushWidth.Size = new System.Drawing.Size(65, 13);
+            this.brushWidth.TabIndex = 6;
+            this.brushWidth.Text = "Brush Width";
+            // 
+            // brushHeightLabel
+            // 
+            this.brushHeightLabel.AutoSize = true;
+            this.brushHeightLabel.Location = new System.Drawing.Point(894, 137);
+            this.brushHeightLabel.Name = "brushHeightLabel";
+            this.brushHeightLabel.Size = new System.Drawing.Size(68, 13);
+            this.brushHeightLabel.TabIndex = 8;
+            this.brushHeightLabel.Text = "Brush Height";
+            // 
+            // heightTrackBar
+            // 
+            this.heightTrackBar.Location = new System.Drawing.Point(876, 153);
+            this.heightTrackBar.Maximum = 100;
+            this.heightTrackBar.Minimum = 1;
+            this.heightTrackBar.Name = "heightTrackBar";
+            this.heightTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.heightTrackBar.TabIndex = 7;
+            this.heightTrackBar.Value = 1;
+            this.heightTrackBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.heightTrackBar_MouseUp);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(857, 195);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(148, 33);
+            this.openButton.TabIndex = 9;
+            this.openButton.Text = "Open...";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(922, 597);
+            this.ClientSize = new System.Drawing.Size(1039, 597);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.brushHeightLabel);
+            this.Controls.Add(this.heightTrackBar);
+            this.Controls.Add(this.brushWidth);
+            this.Controls.Add(this.widthTrackBar);
+            this.Controls.Add(this.toolsHeader);
+            this.Controls.Add(this.changeColorButton);
             this.Controls.Add(this.canvas);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.clearButton);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.widthTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightTrackBar)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +181,15 @@
         private System.Windows.Forms.Button clearButton;
         private System.Windows.Forms.Button exitButton;
         private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Button changeColorButton;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label toolsHeader;
+        private System.Windows.Forms.TrackBar widthTrackBar;
+        private System.Windows.Forms.Label brushWidth;
+        private System.Windows.Forms.Label brushHeightLabel;
+        private System.Windows.Forms.TrackBar heightTrackBar;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- LGPaint/Form1.cs
+++ LGPaint/Form1.cs
@@ -14,14 +14,28 @@ namespace LGPaint
     {
         //Fields
         bool paint = false;
-        SolidBrush color;
+
+        //Used to paint
+        SolidBrush brush;
+        Color color = Color.Black;
+
+        //Width and height of the brush
+        int width = 10;
+        int height = 10;
 
         public Form1()
         {
             InitializeComponent();
 
+            //Change the title of the window
+            this.Text = "Lost Guy Paint";
+
             //Change canvas' background
             canvas.BackColor = Color.White;
+
+            //Set the track bars to the default width and height
+            widthTrackBar.Value = width;
+            heightTrackBar.Value = height;
         }
 
         //Close the program
@@ -58,17 +72,54 @@ namespace LGPaint
             if(paint)
             {
                 //Pick a color
-                //ToDo: Add more colors
-                color = new SolidBrush(Color.Black);
+                brush = new SolidBrush(color);
 
                 Graphics g = canvas.CreateGraphics();
 
                 //Paint on the mouse location
-                //ToDo: Allow a size changer
-                g.FillEllipse(color, e.X, e.Y, 500, 10);
+                g.FillEllipse(brush, e.X, e.Y, width, height);
                 g.Dispose();
+            }
+        }
 
+        //Opens up a ColorDialog for the user to change colors
+        private void changeColorButton_Click(object sender, EventArgs e)
+        {
+            //Open the ColorDialog
+            DialogResult result = colorDialog1.ShowDialog();
 
+            //Check to see if the user clicked OK
+            if (result == DialogResult.OK)
+            {
+                this.color = colorDialog1.Color;
+            }
+
+        }
+
+        //Change the brush's width
+        private void widthTrackBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.width = widthTrackBar.Value;
+        }
+
+        //Change the brush's height
+        private void heightTrackBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.height = heightTrackBar.Value;
+        }
+
+        //Opens a file explorer so the user can open an image
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            //Set the dialogs filter
+            openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+            //Set the background image to what the user selected
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Scale the image upon load to fit the canvas
+                //ToDo: Make a scaling canvas
+                this.canvas.BackgroundImage = new Bitmap(Image.FromFile(openFileDialog1.FileName), new Size(canvas.Width, canvas.Height));
             }
         }
     }
