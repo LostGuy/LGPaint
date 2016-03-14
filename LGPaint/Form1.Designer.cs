0b2578f2bd8eaff51c4768dcd3c8c352873a48e0
Jesse West <jwest46@uncc.edu>
2016-03-13 21:53:02 -0400

    Basic creation. Allows simple drawing.

--- LGPaint/Form1.Designer.cs
+++ LGPaint/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(829, 475);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(81, 52);
+            this.clearButton.TabIndex = 0;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(829, 533);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(81, 52);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(13, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(803, 572);
+            this.canvas.TabIndex = 2;
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 597);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Panel canvas;
     }
 }
 
--- LGPaint/Form1.cs
+++ LGPaint/Form1.cs
@@ -12,9 +12,64 @@ namespace LGPaint
 {
     public partial class Form1 : Form
     {
+        //Fields
+        bool paint = false;
+        SolidBrush color;
+
         public Form1()
         {
             InitializeComponent();
+
+            //Change canvas' background
+            canvas.BackColor = Color.White;
+        }
+
+        //Close the program
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //Create grapchis object
+            Graphics graphics = canvas.CreateGraphics();
+
+            //Clear the canvas
+            graphics.Clear(canvas.BackColor);
+        }
+
+        //MouseUp event for the panel, turns paint to false to quit painting
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+        }
+
+        //MouseDown event for the panel, turns paint to true to start painting
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+        }
+
+        //MouseMove event for the panel
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Paint if the mouse is down
+            if(paint)
+            {
+                //Pick a color
+                //ToDo: Add more colors
+                color = new SolidBrush(Color.Black);
+
+                Graphics g = canvas.CreateGraphics();
+
+                //Paint on the mouse location
+                //ToDo: Allow a size changer
+                g.FillEllipse(color, e.X, e.Y, 500, 10);
+                g.Dispose();
+
+
+            }
         }
     }
 }
